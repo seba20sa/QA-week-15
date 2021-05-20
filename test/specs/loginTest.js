39a208da0de093ea31c7afd912b6101407452f24
Sebastian sileoni <seba20@gmail.com>
2021-05-19 21:50:17 -0300

    finished the first draft of tests for the login page

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -20,22 +20,19 @@ describe (
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
             .toHaveText("Epic sadface: Username is required");
-            // expect(wrongUserInput.isDisplayed()).toBe('true');
-                        
+            // expect(wrongUserInput.isDisplayed()).toBe('true');                        
         });
         it('username: undefined', () => {            
             LoginPage.setUserName(undefined);
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
-            .toHaveText("Epic sadface: Username is required");
-                       
+            .toHaveText("Epic sadface: Username is required");                       
         });  
         it('username not found on the valid credentials list', () => {            
             LoginPage.setUserName('asddassda');
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
-            .toHaveText("Epic sadface: Password is required");
-                       
+            .toHaveText("Epic sadface: Password is required");                       
         });                
     });
     describe ('password field testing', () => {
@@ -58,39 +55,53 @@ describe (
         }); 
     });
     describe ('usernames AND password testing', () => {
-        afterAll('clean browser', () => {
+        beforeAll('clean browser', () => {
             browser.refresh();
-            browser.pause(1500);
+        });
+        afterAll('pause browser', () => {            
+            browser.pause(500);
         });        
-        it('empty username and empty password', () => {            
+        it('empty username and empty password', () => {                        
             LoginPage.testLogin('', '');
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
+            .toHaveText("Epic sadface: Username is required");
+            browser.url(urlLogin);                                  
+        });
+        it('valid username and empty password', () => {            
+            LoginPage.testLogin('standard_user', '');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.errorMessageContainer)
             .toHaveText("Epic sadface: Password is required");
-                                  
+            browser.url(urlLogin);                                  
         });
         it('empty username and credited password', () => {            
             LoginPage.testLogin('', 'secret_sauce');
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
             .toHaveText(
+                "Epic sadface: Username is required"
+            );                                  
+        });        
+        it('invalid username and invalid password', () => {            
+            LoginPage.testLogin('user', 'password123');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText(
                 "Epic sadface: Username and password do not match any user in this service"
-            );
-                                  
-        });                       
+            );                                  
+        });                         
         it('locked user with correct password we stay on the'+
             'login page and get an error message', () => {            
             LoginPage.testLogin('locked_out_user', 'secret_sauce');
             LoginPage.loginBtn.click();
             expect(LoginPage.errorMessageContainer)
-            .toHaveText("Epic sadface: Sorry, this user has been locked out.");
-                                  
+            .toHaveText("Epic sadface: Sorry, this user has been locked out.");                                  
         });
         it('standar  username and password, loads the next url all'+
         ' the pictures are not the dog ones', () => {            
             LoginPage.testLogin('standard_user', 'secret_sauce');            
-            expect(browser).toHaveUrl(urlInventory);
-            
+            expect(browser).toHaveUrl(urlInventory);            
             browser.url(urlLogin);                
         }); 
         it('problematic username and password, loads the next url all'+
@@ -100,11 +111,15 @@ describe (
             expect(InventoryPage.problematicImageSource).toBe(urlDogImg);
             browser.url(urlLogin);                         
         });
-    // it('performance glitch user out username and password, loads the next url all'+
-    // ' the pictures are not the dog ones but it takes a long time to do it', () => {            
-    //         LoginPage.testLogin('performance_glitch_user', 'secret_sauce');
-    //         LoginPage.loginBtn.click();               
-    //     });
+    it('performance glitch user out username and password, loads the next url all'+
+    ' the pictures are not the dog ones but it takes a long time to do it', () => {            
+            LoginPage.testLogin('performance_glitch_user', 'secret_sauce');            
+            browser.setTimeout({
+                'pageLoad': 5000,
+            });
+            expect(browser).toHaveUrl(urlInventory);
+            browser.url(urlLogin);               
+        });
     });   
 });
 
