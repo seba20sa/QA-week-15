5ae1568f0d33773d49fe59a07fadad6f7f69d86c
Sebastian sileoni <seba20@gmail.com>
2021-05-24 15:57:09 -0300

    added tests to the cart section

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -10,27 +10,52 @@ class CartPage {
     get logOutBtn () { return $('#logout_sidebar_link') }
     get resetAppStateBtn () { return $('#reset_sidebar_link') }
     get closeMenuBtn () { return $('#react-burger-cross-btn') }
-    /*Item*/
-    get backpackItemImg () { return $('.inventory_item_img') }
-    get backpackItemtName () { return $('.inventory_item_name') }    
-    get backpackItemDescription () { return $('.inventory_item_desc') }
-    get addBackPackToCart () { return $('#add-to-cart-sauce-labs-backpack') }
-    get removeBackPackFromCart () { return $('#remove-sauce-labs-backpack') }
-    get backpackItemPrice () { return $('.inventory_item_price') }
-    get continueShopping () { return $('#continue-shopping') }    
     get checkOutBtn () { return $('#checkout') }
     get cartQuantity () { return $('.cart_quantity') }
+    get continueShopping () { return $('#continue-shopping') }
+    /*Items*/
+    /*Backpack ID: 4, img:1, name:0, desc:0, price:0*/
+    get addBackPackToCart () { return $('#add-to-cart-sauce-labs-backpack') }
+    get removeBackPackFromCart () { return $('#remove-sauce-labs-backpack') }    
+    /*Lab light ID: 0, img:2, name:1, desc:1, price:1*/    
+    get addLabLightToCart () { return $('#add-to-cart-sauce-labs-bike-light') }
+    get removeLabLightFromCart () { return $('#remove-sauce-labs-bike-light') }    
+    /*Bolt T-shirt ID: 1, img: 4, name: 2, desc: 2, price: 2*/    
+    get addBoltTshirtToCart () { return $('#add-to-cart-sauce-labs-bolt-t-shirt') }
+    get removeBoltTshirtsFromCart () { return $('#remove-sauce-labs-bolt-t-shirt') }    
+    /*Fleece jacket ID: 5, img: 6, name: 3, desc: 3, price: 3*/    
+    get addFleeceJacketToCart () { return $('#add-to-cart-sauce-labs-fleece-jacket') }
+    get removeFleeceJacketFromCart () { return $('#remove-sauce-labs-fleece-jacket') }    
+    /*Onsie ID: 2, img: 8, name: 4, desc: 4, price: 4*/    
+    get addOnsieToCart () { return $('#add-to-cart-sauce-labs-onesie') }
+    get removeOnsieFromCart () { return $('#remove-sauce-labs-onesie') }    
+    /*Red shirt ID: 3, img: 10, name: 5, desc: 5, price: 5*/    
+    get addRedShirtToCartOnInventory () { return $$('.btn_primary')[5] }
+    get addRedShirtToCartOnItems () { return $('.btn_primary')  }      
+    get removeRedShirtFromCartOnInventory () { return $$('.btn_secondary')[5]}
+    get removeRedShirtFromCartOnItems  () { return $('.btn_small') }
     /*SETTERS*/
+    nameItemSelector (value) {
+        return $$('.inventory_item_name')[value]
+    }
+    igmItemSelector (value) {
+        return $$('.inventory_item_img')[value]
+    }
+    descriptionItemSelector (value) {
+        return $$('.inventory_item_desc')[value]
+    }
+    priceItemSelector (value) {
+        return $$('.inventory_item_price')[value]
+    }
+    sortItems (value) {
+        this.productSorter.click();
+        this.productSorter.$$('option')[`${value}`].click();
+    }
     burgerMenuOption (option) {
         this.burgerMenuBtn.click();
         browser.pause(1000);
         option.click();
     }
-
-           
-       
     /* METHODS */
-
-   
 }    
 module.exports = new CartPage();
--- test/specs/cartTest.js
+++ test/specs/cartTest.js
@@ -1,7 +1,7 @@
 const LoginPage = require('../pageobjects/login.page');
 const InventoryPage = require('../pageobjects/inventory.page');
 const CartPage = require('../pageobjects/cart.page');
-describe ('',  () => {    
+describe ('CART page tests',  () => {    
     /*URLs to perform the test*/
     const urlLogin = 'https://www.saucedemo.com/';
     const urlInventory = 'https://www.saucedemo.com/inventory.html';
@@ -12,41 +12,190 @@ describe ('',  () => {
         'then click the cart icon', () => {
             browser.url(urlLogin);
             LoginPage.testLogin('standard_user', 'secret_sauce');
-            browser.pause(1000);
-            InventoryPage.addBackPackToCart.click();            
-            InventoryPage.shoppingCartLink.click();
         });                  
-    describe ('Tests performed from the cart page', () => {                
+    describe ('Tests performed from BACK-PACK item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addBackPackToCart.click();            
+            InventoryPage.shoppingCartLink.click();            
+        });                
         it('Check if the URL, item quantity and item info are correct', () => {
             expect(browser).toHaveUrl(urlCart);          
             expect(CartPage.cartQuantity).toHaveText("1");
-            expect(CartPage.backpackItemtName).toHaveText("Sauce Labs Backpack");
-            expect(CartPage.backpackItemDescription)
+            expect(CartPage.nameItemSelector(0)).toHaveText("Sauce Labs Backpack");
+            expect(CartPage.descriptionItemSelector(0))
             .toHaveText("carry.allTheThings() with the sleek, streamlined Sly Pack that"+
             " melds uncompromising style with unequaled laptop and tablet protection.");
-            expect(CartPage.backpackItemPrice).toHaveText("$29.99");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$29.99");
         });        
         it('From the cart page click CONTINUE SHOPPING', () => {
             CartPage.continueShopping.click();
             expect(browser).toHaveUrl(urlInventory);
             browser.pause(1000);
         });
-        
-        
         it('Remove item and check the quantity and cart counter ', () => {
             CartPage.removeBackPackFromCart.click();            
             expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
-            expect(CartPage.cartQuantity).not.toBeDisplayed();
-                      
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
         });
         it('Reset app state', () => {
             CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
             expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
             expect(CartPage.cartQuantity).not.toBeDisplayed();
-                      
         });                 
     });
-      
+    describe ('Tests performed from BIKE-LIGHT item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addLabLightToCart.click();            
+            InventoryPage.shoppingCartLink.click();            
+        });                
+        it('Check if the URL, item quantity and item info are correct selector is ZERO', () => {
+            expect(browser).toHaveUrl(urlCart);          
+            expect(CartPage.cartQuantity).toHaveText("1");
+            expect(CartPage.nameItemSelector(0))
+            .toHaveText("Sauce Labs Bike Light");
+            expect(CartPage.descriptionItemSelector(0))
+            .toHaveText("A red light isn't the desired state in testing but it sure helps when "+
+            "riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$9.99");
+        });        
+        it('From the cart page click CONTINUE SHOPPING', () => {
+            CartPage.continueShopping.click();
+            expect(browser).toHaveUrl(urlInventory);
+            browser.pause(1000);
+        });
+        it('Remove item and check the quantity and cart counter ', () => {
+            CartPage.removeLabLightFromCart.click();            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
+        });
+        it('Reset app state', () => {
+            CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();
+        });                 
+    });
+    describe ('Tests performed from BOLT SHIRT item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addBoltTshirtToCart.click();            
+            InventoryPage.shoppingCartLink.click();            
+        });                
+        it('Check if the URL, item quantity and item info are correct selector is ZERO', () => {
+            expect(browser).toHaveUrl(urlCart);          
+            expect(CartPage.cartQuantity).toHaveText("1");
+            expect(CartPage.nameItemSelector(0)).toHaveText("Sauce Labs Bolt T-Shirt");
+            expect(CartPage.descriptionItemSelector(0))
+            .toHaveText("Get your testing superhero on with the Sauce Labs bolt T-shirt."+
+            " From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$15.99");
+        });        
+        it('From the cart page click CONTINUE SHOPPING', () => {
+            CartPage.continueShopping.click();
+            expect(browser).toHaveUrl(urlInventory);
+            browser.pause(1000);
+        });
+        it('Remove item and check the quantity and cart counter ', () => {
+            CartPage.removeBoltTshirtsFromCart.click();            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
+        });
+        it('Reset app state', () => {
+            CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();
+        });                 
+    });
+    describe ('Tests performed from FLEECE JACKET item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addFleeceJacketToCart.click();            
+            InventoryPage.shoppingCartLink.click();            
+        });                
+        it('Check if the URL, item quantity and item info are correct selector is ZERO', () => {
+            expect(browser).toHaveUrl(urlCart);          
+            expect(CartPage.cartQuantity).toHaveText("1");
+            expect(CartPage.nameItemSelector(0)).toHaveText("Sauce Labs Fleece Jacket");
+            expect(CartPage.descriptionItemSelector(0))
+            .toHaveText("It's not every day that you come across a midweight quarter-zip fleece"+
+            " jacket capable of handling everything from"+
+            " a relaxing day outdoors to a busy day at the office.");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$49.99");
+        });        
+        it('From the cart page click CONTINUE SHOPPING', () => {
+            CartPage.continueShopping.click();
+            expect(browser).toHaveUrl(urlInventory);
+            browser.pause(1000);
+        });
+        it('Remove item and check the quantity and cart counter ', () => {
+            CartPage.removeFleeceJacketFromCart.click();            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
+        });
+        it('Reset app state', () => {
+            CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();
+        });                 
+    });
+    describe ('Tests performed from ONSIE item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addOnsieToCart.click();            
+            InventoryPage.shoppingCartLink.click();            
+        });                
+        it('Check if the URL, item quantity and item info are correct selector is ZERO', () => {
+            expect(browser).toHaveUrl(urlCart);          
+            expect(CartPage.cartQuantity).toHaveText("1");
+            expect(CartPage.nameItemSelector(0)).toHaveText("Sauce Labs Onesie");
+            expect(CartPage.descriptionItemSelector(0))
+            .toHaveText("Rib snap infant onesie for the junior automation engineer in development."+
+            " Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$7.99");
+        });        
+        it('From the cart page click CONTINUE SHOPPING', () => {
+            CartPage.continueShopping.click();
+            expect(browser).toHaveUrl(urlInventory);
+            browser.pause(1000);
+        });
+        it('Remove item and check the quantity and cart counter ', () => {
+            CartPage.removeOnsieFromCart.click();            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
+        });
+        it('Reset app state', () => {
+            CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();
+        });                 
+    });
+    describe ('Tests performed from RED SHIRT item from the cart page', () => {
+        beforeAll('from the inventory page add the item to the cart and access the cart', () =>{
+            InventoryPage.addRedShirtToCartOnInventory.click();                     
+            InventoryPage.shoppingCartLink.click();                          
+        });                
+        it('Check if the URL, item quantity and item info are correct selector is ZERO', () => {
+            expect(browser).toHaveUrl(urlCart);          
+            expect(CartPage.cartQuantity).toHaveText("1");
+            expect(CartPage.nameItemSelector(0))
+            .toHaveText("Test.allTheThings() T-Shirt (Red)");
+            expect(CartPage.descriptionItemSelector(0))
+            .toHaveText("This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.");
+            expect(CartPage.priceItemSelector(0)).toHaveText("$15.99");
+        });
+        it('Remove item and check the quantity and cart counter ', () => {
+            browser.pause(5000);
+            CartPage.removeRedShirtFromCartOnItems.click();            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();                      
+        });
+        it('From the cart page click CONTINUE SHOPPING', () => {
+            CartPage.continueShopping.click();
+            expect(browser).toHaveUrl(urlInventory);
+            browser.pause(1000);
+        });
+        it('Reset app state', () => {
+            CartPage.burgerMenuOption(CartPage.resetAppStateBtn);            
+            expect(CartPage.cartItemsCounter).not.toBeDisplayed();            
+            expect(CartPage.cartQuantity).not.toBeDisplayed();
+        });                 
+    });          
 });
 
 
